02d0e9fcad2224965c6f663f88f675b1f3480dc1
zenonxd <olavomoreira99@gmail.com>
2024-07-30 15:47:42 -0300

    update

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -1,13 +1,18 @@
 package com.devsuperior.aula;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AulaApplication {
+public class AulaApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(AulaApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
